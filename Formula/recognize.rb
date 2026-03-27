b694228cddf7f92705cdabaa0b0a3981d7062329
Nick Cao <nick.cao@avcrm.net>
2026-03-27 16:23:32 +1030

    feat: add recognize formula v2.2.0

---
+++ Formula/recognize.rb
@@ -0,0 +1,44 @@
+class Recognize < Formula
+  desc "Real-time speech recognition with CoreML acceleration for macOS"
+  homepage "https://github.com/xicv/recognize"
+  url "https://github.com/xicv/recognize/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "78dd142f3d7ff453a2b9aceb4fbf8fddaf203e766ab49b3863e83e6310aee6ec"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "sdl2"
+  depends_on :macos
+
+  def install
+    # Initialize whisper.cpp submodule
+    system "git", "init"
+    system "git", "submodule", "update", "--init", "--recursive", "fixtures/whisper.cpp" rescue nil
+
+    cd "src/cli" do
+      mkdir "build" do
+        system "cmake", "..",
+               "-DWHISPER_COREML=ON",
+               "-DWHISPER_COREML_ALLOW_FALLBACK=ON",
+               "-DGGML_METAL=ON",
+               "-DGGML_METAL_EMBED_LIBRARY=ON",
+               "-DGGML_NATIVE=ON",
+               "-DGGML_ACCELERATE=ON",
+               "-DGGML_BLAS=ON",
+               "-DGGML_LTO=ON",
+               "-DWHISPER_SDL2=ON",
+               *std_cmake_args
+        system "make", "-j#{ENV.make_jobs}"
+      end
+
+      bin.install "build/recognize"
+    end
+  end
+
+  def post_install
+    system "mkdir", "-p", "#{ENV["HOME"]}/.recognize/models", "#{ENV["HOME"]}/.recognize/tmp"
+  end
+
+  test do
+    assert_match "usage:", shell_output("#{bin}/recognize --help 2>&1", 0)
+  end
+end
